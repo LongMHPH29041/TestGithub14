93b10881abd4d8195e34babf8a6bbbfb8a7db9f2
LE TIEN THANH <thanhltph29166@fpt.edu.vn>
2023-03-16 22:46:28 +0700

    ph29166

---
+++ app/src/main/java/h2clt/fpt/testgithub14/ThanhPH29166.kt
@@ -0,0 +1,11 @@
+package h2clt.fpt.testgithub14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThanhPH29166 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thanh_ph29166)
+    }
+}
