eccdb38f1e715f63cd6aa1f4c1291335a4fafe8c
LE TIEN THANH <thanhltph29166@fpt.edu.vn>
2023-03-16 22:56:37 +0700

    abccc

---
+++ app/src/main/java/h2clt/fpt/testgithub14/LanCuoi.kt
@@ -0,0 +1,11 @@
+package h2clt.fpt.testgithub14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LanCuoi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lan_cuoi)
+    }
+}
